c501aecf18b2664d2f961d2a90736fcdb58a2649
Youssef Nafaa <nafaa.youssef@yahoo.fr>
2023-12-14 22:12:02 +0100

    backend setup
    Signed-off-by: Youssef Nafaa <nafaa.youssef@yahoo.fr>

---
+++ app/controllers/leagues_controller.rb
@@ -0,0 +1,22 @@
+class LeaguesController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def index
+  end
+end
---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,19 @@
+class TeamsController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
