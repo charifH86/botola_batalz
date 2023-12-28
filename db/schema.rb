0062c92ea02f935b7b9269dd2f2c8f5fa58f28dd
Youssef Nafaa <nafaa.youssef@yahoo.fr>
2023-12-28 20:36:00 +0100

    work in p

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_12_28_131045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_28_131045) do
   create_table "players", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.integer "price"
     t.string "height"
     t.string "nationality"
     t.string "position"
@@ -73,7 +73,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_28_131045) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price"
     t.string "poste", default: ""
   end
 
@@ -92,8 +91,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_28_131045) do
     t.bigint "league_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "budget", default: 5000000
     t.bigint "user_id", null: false
+    t.integer "budget", default: 5000000
     t.index ["league_id"], name: "index_teams_on_league_id"
     t.index ["user_id"], name: "index_teams_on_user_id"
   end
