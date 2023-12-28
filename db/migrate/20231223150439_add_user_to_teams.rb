ec8aee224d4bd8e420de4f340a788ecf86a05c3a
Charif Hassouni <hassounimohamedc@gmail.com>
2023-12-28 13:29:28 +0100

    ajout liste déroulantepar position
    Signed-off-by: Charif Hassouni <hassounimohamedc@gmail.com>

--- db/migrate/20231223150439_add_user_to_teams.rb
+++ db/migrate/20231223150439_add_user_to_teams.rb
@@ -1,15 +1,8 @@
 class AddUserToTeams < ActiveRecord::Migration[7.0]
   def change
-    # Step 1: Add the user_id column without null: false constraint
     add_reference :teams, :user, foreign_key: true
-
-    # Step 2: Assuming you have at least one user
     default_user_id = User.first.id
-
-    # Update all teams without a user
     Team.where(user_id: nil).update_all(user_id: default_user_id)
-
-    # Step 3: Change the column to not allow null
     change_column_null :teams, :user_id, false
   end
 end
