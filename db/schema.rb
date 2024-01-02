54cf6d2764c82d50b3e7d724d5eb97deebed259a
Charif Hassouni <hassounimohamedc@gmail.com>
2024-01-02 22:24:40 +0100

    updatetravauxencours
    Signed-off-by: Charif Hassouni <hassounimohamedc@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_28_131045) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_02_201423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_28_131045) do
     t.bigint "league_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "pending"
+    t.date "date"
+    t.string "endgame", default: "draw"
     t.index ["league_id"], name: "index_games_on_league_id"
   end
 
@@ -81,6 +84,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_28_131045) do
     t.bigint "player_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "position", default: "0"
+    t.integer "score", default: 0
     t.index ["player_id"], name: "index_team_players_on_player_id"
     t.index ["team_id"], name: "index_team_players_on_team_id"
   end
